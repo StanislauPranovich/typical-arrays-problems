f2f1c9126801a56095eb697d62cc92e29f188d47
Stanislau Pranovich <stanislau.pranovich@gmail.com>
2020-10-03 12:04:36 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === 0) {
+    return 0;
+  } else if (array === undefined) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {    
+    return Math.min(...array)
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === 0) {
+    return 0;
+  } else if (array === undefined) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {    
+    return Math.max(...array)
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+  if(array === 0) {
+    return 0;
+  } else if (array === undefined) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+    for (let arg of array) {
+    sum += arg;
+    }
+    return sum/array.length;
+  } 
+  }
